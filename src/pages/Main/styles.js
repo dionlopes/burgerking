ea76f2a876125d4a4a8276f906112ec51571be68
dionlopes <dion.mello@hotmail.com>
2019-07-13 22:08:13 -0300

    chore: Estilização responsiva na listagem

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -10,9 +10,21 @@ export const Container = styled.div`
 
 export const Products = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 20px;
   padding-bottom: 20px;
+
+  @media screen and (min-width: 640px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (min-width: 960px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (min-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 
 export const CategoryTitle = styled.h2`
